6f39831fd8c3822ba7a35e64ad764c0f2f7cb4c5
Murat Öz <ozmurat@windowslive.com>
2023-06-05 14:32:21 +0300

    Added an ability to use jacks more efficiently to bots

--- Assets/Scripts/Gameplay/Players/BotPlayer.cs
+++ Assets/Scripts/Gameplay/Players/BotPlayer.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UI;
 using UnityEngine;
+using Utilities;
 
 namespace Gameplay.Players
 {
 	public class BotPlayer : CardPlayer
 	{
+		private List<Card> jacksInHand;
+
 		public override void Setup(PlayerSlot _playerSlot)
 		{
 			Money = Random.Range(5, 50) * 100;
@@ -25,6 +29,20 @@ namespace Gameplay.Players
 
 			if (Board.Instance.CardsInBoard.Count > 0)
 			{
+				// if has at least one jack & board has a score play with jack
+				if (jacksInHand.Count > 0)
+				{
+					for (int i = 0; i < Board.Instance.CardsInBoard.Count; i++)
+					{
+						var cardInBoard = Board.Instance.CardsInBoard[i];
+						if (cardInBoard.IsOpen && cardInBoard.CardSO.Score > 0)
+						{
+							PlayCard(jacksInHand[^1]);
+							yield break;
+						}
+					}
+				}
+
 				for (int i = 0; i < Hand.Count; i++)
 				{
 					// Looks if can fish cards with same card
@@ -36,13 +54,35 @@ namespace Gameplay.Players
 				}
 			}
 
-			PlayCard(Hand[Random.Range(0, Hand.Count)]);
+			var randomCard = Hand[Random.Range(0, Hand.Count)];
+			// To not drop the jack on an empty board
+			while (randomCard.CardSO.CardRank == CardRank.Jack && Board.Instance.CardsInBoard.Count.Equals(0) && !jacksInHand.Count.Equals(Hand.Count))
+			{
+				randomCard = Hand[Random.Range(0, Hand.Count)];
+			}
+
+			PlayCard(randomCard);
 		}
 
 		public override void PlayCard(Card card)
 		{
 			card.Open();
+			if (card.CardSO.CardRank == CardRank.Jack)
+				jacksInHand.Remove(card);
+
 			base.PlayCard(card);
+		}
+
+		public override void OnCardsDealt()
+		{
+			base.OnCardsDealt();
+			jacksInHand = new List<Card>();
+
+			foreach (var card in Hand)
+			{
+				if (card.CardSO.CardRank == CardRank.Jack)
+					jacksInHand.Add(card);
+			}
 		}
 	}
 }
